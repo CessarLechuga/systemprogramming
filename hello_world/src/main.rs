2692beac44d6609c1bee17644d70ebde8eb08658
CessarLechuga <143654625+CessarLechuga@users.noreply.github.com>
2024-09-11 15:47:30 +0000

    Comments

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,76 @@
+/*const FREEZING_POINT_F: f64 = 32.0;
+
+fn fahrenheit_to_celsius(f: f64) -> f64 {
+    (f - FREEZING_POINT_F) * 5.0 / 9.0
+}
+
+fn celsius_to_fahrenheit(c: f64) -> f64 {
+    c * 9.0 / 5.0 + FREEZING_POINT_F
+}
+
+fn main() {
+    let mut temperature_f = 32.0;
+    
+    println!("{}°F is equal to {:.2}°C", temperature_f, fahrenheit_to_celsius(temperature_f));
+    
+    for _ in 0..5 {
+        temperature_f += 1.0;
+        println!("{}°F is equal to {:.2}°C", temperature_f, fahrenheit_to_celsius(temperature_f));
+    }
+}
+    */
+
+    /*
+    fn is_even(n: i32) -> bool {
+        n % 2 == 0
+    }
+    
+    fn main() {
+        let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        
+        for &num in &numbers {
+            if num % 3 == 0 && num % 5 == 0 {
+                println!("FizzBuzz");
+            } else if num % 3 == 0 {
+                println!("Fizz");
+            } else if num % 5 == 0 {
+                println!("Buzz");
+            } else if is_even(num) {
+                println!("{} is even", num);
+            } else {
+                println!("{} is odd", num);
+            }
+        }
+        
+        let mut sum = 0;
+        let mut i = 0;
+        while i < numbers.len() {
+            sum += numbers[i];
+            i += 1;
+        }
+        println!("Sum of all numbers: {}", sum);
+        
+        let mut largest = numbers[0];
+        let mut j = 1;
+        loop {
+            if j >= numbers.len() {
+                break;
+            }
+            if numbers[j] > largest {
+                largest = numbers[j];
+            }
+            j += 1;
+        }
+        println!("Largest number: {}", largest);
+    }
+*/    
+
+
+
+
+
+
+
 fn check_guess(guess: i32, secret: i32) -> i32 {
    if guess == secret {
        0
